34341e7c71e96faf5cb2699245bb655d859343cc
Felipe Matos <88333095+FMTSL@users.noreply.github.com>
2023-03-12 16:08:18 -0300

    Entidades JPA

---
+++ Primeiro Projeto/Hello Word/src/main/java/med/voll/api/paciente/Paciente.java
@@ -0,0 +1,25 @@
+package med.voll.api.paciente;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import med.voll.api.endereco.Endereco;
+
+@Table(name = "pacientes")
+@Entity(name = "Paciente")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Paciente {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String nome;
+    private String email;
+    private int cpf;
+    @Embedded
+    private Endereco endereco;
+}
