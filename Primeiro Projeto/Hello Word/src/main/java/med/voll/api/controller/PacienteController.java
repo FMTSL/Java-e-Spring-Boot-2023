2dc72aa45858f7fea83b99c36a0271e0250babfe
Felipe Matos <88333095+FMTSL@users.noreply.github.com>
2023-03-13 12:52:55 -0300

    Migration|Validation

--- Primeiro Projeto/Hello Word/src/main/java/med/voll/api/controller/PacienteController.java
+++ Primeiro Projeto/Hello Word/src/main/java/med/voll/api/controller/PacienteController.java
@@ -1,5 +1,7 @@
 package med.voll.api.controller;
 
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
 import med.voll.api.paciente.DadosCadastroPaciente;
 import med.voll.api.paciente.Paciente;
 import med.voll.api.paciente.PacienteRepository;
@@ -16,7 +18,8 @@ public class PacienteController {
     @Autowired
     private PacienteRepository repository;
     @PostMapping
-    public void cadastrar(@RequestBody DadosCadastroPaciente dadosPaciente){
+    @Transactional
+    public void cadastrar(@RequestBody @Valid DadosCadastroPaciente dadosPaciente){
         repository.save(new Paciente(dadosPaciente));
     }
 }
