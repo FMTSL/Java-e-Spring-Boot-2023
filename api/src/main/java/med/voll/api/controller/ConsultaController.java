25f15bda379ee541d267b600a2f4734eac4fb54e
FMTSL <felipe_matos@hotmail.com.br>
2023-07-05 14:30:38 -0300

    Class Service

--- api/src/main/java/med/voll/api/controller/ConsultaController.java
+++ api/src/main/java/med/voll/api/controller/ConsultaController.java
@@ -1,6 +1,7 @@
 package med.voll.api.controller;
 
 import jakarta.transaction.Transactional;
+import med.voll.api.domain.consulta.AgendaDeConsultas;
 import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
 import med.voll.api.domain.consulta.DadosDetalhamentoConsulta;
 import org.springframework.http.ResponseEntity;
@@ -12,9 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("consultas")
 public class ConsultaController {
 
+    private AgendaDeConsultas agenda;
+
     @PostMapping
     @Transactional
     public ResponseEntity agendar (DadosAgendamentoConsulta dados){
+        agenda.agendar(dados);
         return ResponseEntity.ok(new DadosDetalhamentoConsulta( null, null, null, null));
     }
 }
